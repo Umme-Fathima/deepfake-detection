1b81ac075fe4854b0a6d093e8e7652e3b19978c2
Umme-Fathima <167801316+Umme-Fathima@users.noreply.github.com>
2025-02-27 11:36:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -151,7 +151,7 @@ def get_db_connection():
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        port=int(os.getenv("DB_PORT")),
+        port=int(os.getenv("DB_PORT", 56496)),
         database=os.getenv("DB_NAME")
     )
 
